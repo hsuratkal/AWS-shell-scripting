6492166154feac26a649940ccf7bdba805210cc9
Suratkal <hsuratkal@nyls.edu>
2022-12-30 11:51:32 -0500

    added/edited robot project frontend file

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -2,14 +2,14 @@
 
 ID=$(id -u)
 
-if [ $ID -ne 0 ]; then
+if [ $ID -ne 0 ] ; then
    echo -e "\e[31m You need to run the script as a root user or with a sudo privilege\e[0m"
    exit 1
 fi 
 
 echo "Installing Nginx :"
 yum install nginx -y     &>> /tmp/frontend.log
-if [ $? -eq 0]; then
+if [ $? -eq 0] ; then
     echo -e "\e[32mSuccess\e[0m"
 else
     echo -e "\e[31mFailure\e[0m"
