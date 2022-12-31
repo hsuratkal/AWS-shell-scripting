9325ee8fc7394e47ed8fe875aba98736f4d4b6a7
Suratkal <hsuratkal@nyls.edu>
2022-12-30 22:08:06 -0500

    added/edited robot project frontend file

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -23,3 +23,13 @@ if [ $? -eq 0 ] ; then
 else
     echo -e "\e[31mFailure\e[0m"
 fi
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
