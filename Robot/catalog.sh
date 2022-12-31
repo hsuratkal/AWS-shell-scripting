64d40ecf4d2b53cd08819aa7b73a2340838bd925
Suratkal <hsuratkal@nyls.edu>
2022-12-31 13:50:09 -0500

    added/edited robot project mongodb file

--- Robot/catalog.sh
+++ Robot/catalog.sh
@@ -9,7 +9,7 @@ source Robot/common.sh  # Source loads a file and this file all the common patte
 echo -e "\e[32m ________ $COMPONENT Configuration is Starting _______ \e[0m"
 
 echo -n "Configuring NodeJS Repo : "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
 yum install nodejs -y  &>> $LOGFILE
 stat $?
 
