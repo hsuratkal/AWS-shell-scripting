bb1bb7a18052baa3b1b755d6e83f18fda753edc4
Suratkal <hsuratkal@nyls.edu>
2022-12-30 22:13:09 -0500

    added/edited robot project frontend file

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+COMPONENT=frontend
+
 ID=$(id -u)
 
 if [ $ID -ne 0 ] ; then
@@ -7,29 +9,30 @@ if [ $ID -ne 0 ] ; then
    exit 1
 fi 
 
+stat() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32mSuccess\e[0m"
+    else
+        echo -e "\e[31mFailure\e[0m"
+    fi    
+}
 echo -n "Installing Nginx :"
 yum install nginx -y     &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32mSuccess\e[0m"
-else
-    echo -e "\e[31mFailure\e[0m"
-fi
+stat $?
 
 echo -n "Starting Nginx :"
 systemctl enable nginx   &>> /tmp/frontend.log
 systemctl start nginx    &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32mSuccess\e[0m"
-else
-    echo -e "\e[31mFailure\e[0m"
-fi
+stat $?
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -n "Downloading the $COMPONENT"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
 
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
