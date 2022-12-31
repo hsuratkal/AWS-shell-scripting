4f2369c963548ecf33b4d4ca588ea5d70c20e276
Suratkal <hsuratkal@nyls.edu>
2022-12-30 22:20:00 -0500

    added/edited robot project frontend file

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -29,10 +29,18 @@ echo -n "Downloading the $COMPONENT : "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "Clearing the default content : "
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+echo -n "Extracting $COMPONENT : "
+unzip /tmp/frontend.zip &>> /tmp/$COMPONENT.log
+stat $?
+
+echo -n "Copying $COMPONENT : "
+mv frontend-main/* .  &>> /tmp/$COMPONENT.log
+mv static/* .         &>> /tmp/$COMPONENT.log
+rm -rf frontend-main README.md   &>> /tmp/$COMPONENT.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/$COMPONENT.log 
+
